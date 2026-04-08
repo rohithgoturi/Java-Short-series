351187bf2c14e706da37e46a1a566fa2cdeaca46
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-04-08 23:46:30 +0530

    streak

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;  // Import the Scanner class
 
 class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);  // Create a Scanner object
+    Scanner sc = new Scanner(System.in);  
     System.out.println("Enter username");
 
     String userName = sc.nextLine();  
