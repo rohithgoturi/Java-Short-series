6d82be0be8d102cbad44659dacfa6a5552aa0d5a
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-04-14 23:54:21 +0530

    shorts update

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter username");
+    
     String userName = sc.nextLine();  
     System.out.println("Username is: " + userName);  
 
