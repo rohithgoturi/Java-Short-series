7a254a2b6d30260527283675e290d7692f1dd3d3
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-04-16 22:44:54 +0530

    java short series

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter username");
-    
-    
     String userName = sc.nextLine();  
     System.out.println("Username is: " + userName);  
 
